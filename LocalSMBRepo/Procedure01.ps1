5eca8ae6adaf2341180f7ca671dc1a76fbe14e36
distcode <office@dist.at>
2024-03-19 15:56:05 +0100

    local repo demonstration

---
+++ LocalSMBRepo/Procedure01.ps1
@@ -0,0 +1,9 @@
+﻿
+Register-PSRepository -Name 'LocalSMBRepo' -SourceLocation \\dc1\PSLocalRepo -ScriptSourceLocation \\dc1\PSLocalRepo -InstallationPolicy Trusted
+
+# List current Package Sources
+Get-PackageSource
+
+
+# Publish / Upload module to LocalSBMRepo
+Publish-Module -Path C:\MAPS2403\MAPSTools -Repository LocalSMBRepo -NuGetApiKey 'AnyPossibleKey'
