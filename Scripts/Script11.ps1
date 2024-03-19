5967f38bca77b698f5b43b02a661fb9a2981d653
unknown <Admin@MAPS.LOCAL>
2024-03-19 10:27:09 +0100

    next script ...

---
+++ Scripts/Script11.ps1
@@ -0,0 +1,33 @@
+﻿
+
+$computers = @('Srv1','Srv2','Obelix','Asterix','Idefix')
+
+$AllComputers = @()
+
+foreach ( $tempcomputer in $computers ) {
+
+    $myObject = New-Object -TypeName psobject
+
+    Add-Member -InputObject $myObject -MemberType NoteProperty -Name 'ComputerName' -Value 'Srv1'
+
+    $myObject | Add-Member -MemberType AliasProperty -Name Hostname -Value ComputerName
+
+    $myObject | Add-Member -MemberType ScriptMethod -Name Restart -Value { Write-Host "Rechner $($this.ComputerName) wird neu gestartet ..." -ForegroundColor Cyan }
+    $myObject | Add-Member -MemberType ScriptMethod -Name Shutdown -Value { Write-Host "Rechner $($this.ComputerName) wird herunter gefahren ..." -ForegroundColor Yellow }
+
+    $myObject | Add-Member -MemberType NoteProperty -Name OS -Value 'Windows Server 2022'
+    $myObject | Add-Member -MemberType NoteProperty -Name RAM -Value 12
+    $myObject | Add-Member -MemberType NoteProperty -Name CPU -Value 8
+
+}
+
+
+
+
+$myObject | Get-Member
+
+$myObject
+
+$myObject.Restart()
+$myObject.Shutdown()
+
---
+++ Scripts/Script12.ps1
@@ -0,0 +1,22 @@
+﻿
+
+$computers = @('Srv1','Srv2','Obelix','Asterix','Idefix')
+
+$AllComputers = @()
+
+foreach ( $tempcomputer in $computers ) {
+
+    $myObject = New-Object -TypeName psobject
+
+    $myObject | Add-Member -MemberType NoteProperty -Name 'ComputerName' -Value $tempcomputer
+    $myObject | Add-Member -MemberType NoteProperty -Name OS -Value ( @('Windows Server 2022', 'Linux') | Get-Random -Count 1)
+    $myObject | Add-Member -MemberType NoteProperty -Name RAM -Value ( @(12,18,24) | Get-Random -Count 1)
+    $myObject | Add-Member -MemberType NoteProperty -Name CPU -Value ( @(4,8,12) | Get-Random -Count 1)
+    
+    $myObject | Add-Member -MemberType ScriptMethod -Name Restart -Value { Write-Host "Rechner $($this.ComputerName) wird neu gestartet ..." -ForegroundColor Cyan }
+    $myObject | Add-Member -MemberType ScriptMethod -Name Shutdown -Value { Write-Host "Rechner $($this.ComputerName) wird herunter gefahren ..." -ForegroundColor Yellow }
+    
+    $AllComputers += $myObject
+}
+
+$AllComputers
