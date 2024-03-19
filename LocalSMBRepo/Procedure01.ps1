a841f3c75ce63eddfeb4c70f9d3dba51188e9d51
DIST <office@dist.at>
2024-03-19 17:09:53 +0100

    docu link added

--- LocalSMBRepo/Procedure01.ps1
+++ LocalSMBRepo/Procedure01.ps1
@@ -7,3 +7,5 @@ Get-PackageSource
 
 # Publish / Upload module to LocalSBMRepo
 Publish-Module -Path C:\MAPS2403\MAPSTools -Repository LocalSMBRepo -NuGetApiKey 'AnyPossibleKey'
+
+# MS Documentation: https://learn.microsoft.com/en-us/powershell/gallery/how-to/working-with-local-psrepositories?view=powershellget-3.x
