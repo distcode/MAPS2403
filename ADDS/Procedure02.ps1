53e3117b7f87664b4311645051f4e1aa000c42d5
distcode <office@dist.at>
2024-03-20 08:46:48 +0100

    ou ACL Modify added

---
+++ ADDS/Procedure02.ps1
@@ -0,0 +1,24 @@
+﻿$OrganizationalUnit = "OU=Detectives,DC=maps,DC=local"
+$GroupName = "Domain Users"
+
+# Set-Location AD:
+$Group = Get-ADGroup -Identity $GroupName
+$GroupSID = [System.Security.Principal.SecurityIdentifier]$Group.SID
+$ACL = Get-Acl -Path "AD:\$OrganizationalUnit"
+
+$Identity = [System.Security.Principal.IdentityReference]$GroupSID
+$ADRight = [System.DirectoryServices.ActiveDirectoryRights]::GenericAll
+$Type = [System.Security.AccessControl.AccessControlType]::Allow
+$InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance]::All
+$Rule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule($Identity, $ADRight, $Type,  $InheritanceType)
+
+$ACL.AddAccessRule($Rule)
+Set-Acl -Path "ad:\$OrganizationalUnit" -AclObject $ACL
+
+
+# Docu Link(s)
+# https://learn.microsoft.com/en-us/dotnet/api/system.directoryservices.activedirectoryrights?view=dotnet-plat-ext-8.0
+# https://learn.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.accesscontroltype?view=net-8.0
+# https://learn.microsoft.com/en-us/dotnet/api/system.directoryservices.activedirectoryaccessrule?view=dotnet-plat-ext-8.0
+#
+# https://learn.microsoft.com/en-us/answers/questions/364180/powershell-script-delegate-ou-permissions
