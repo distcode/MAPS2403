96ce8312da3b1cc5fe195895647ea26d8291f52a
distcode <office@dist.at>
2024-03-20 16:21:09 +0100

    ErrorAvoidingHandling initialy added.

---
+++ AdvFunctions/ErrorAvoiding.ps1
@@ -0,0 +1,20 @@
+﻿
+
+#Requires -Modules ABJson
+
+Set-StrictMode -Version latest
+
+if ( -not (Test-Path c:\Tmp)) {
+    
+    md C:\Tmp    
+}
+
+$Processes = Get-Process
+
+if ( $Processes.Name -contains 'regedit' ) {
+
+    return -1
+}
+
+
+get-help about_requires -ShowWindow
---
+++ AdvFunctions/ErrorHandling.ps1
@@ -0,0 +1,29 @@
+﻿
+
+Set-StrictMode -Version latest
+$ErrorActionPreference = 'Stop'
+
+try {
+    $a = 100
+    $b = 25 # Read-Host 'Bitte einen Wert eingeben'
+    
+    $result = $a / $b
+    
+    Get-ChildItem hklm:\ 
+
+    Write-Host "Ergebnis: $result" -BackgroundColor DarkGreen
+}
+catch [System.DivideByZeroException] {
+       "Bitte nicht 0 eingeben ..."
+       # $_.InvocationInfo
+       return 'Ich breche ab ...'
+}
+catch {
+    "Irgendein Fehler ist aufgetreten ..."
+
+    $Error[0] | Format-List -Property * -Force
+    $Error[0].InvocationInfo
+}
+finally {
+    "*** FINISHED ***"
+}
