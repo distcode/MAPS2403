736408c3fa9e8346e895a8076adc94706c47e850
distcode <office@dist.at>
2024-03-20 15:23:24 +0100

    corrected to \Root.

--- AdvFunctions/Signing-CreateSSC.ps1
+++ AdvFunctions/Signing-CreateSSC.ps1
@@ -12,7 +12,7 @@ $cert = get-ChildItem Cert:\CurrentUser\My\D1E9EFAF402A1D8A102763A0A424732B50311
 
 # Export public key / root certificate
 Export-Certificate -Type CERT -FilePath C:\Temp\rootSSC-01.cer -Cert $cert
-Import-Certificate -FilePath C:\Temp\rootSSC-01.cer -CertStoreLocation Cert:\CurrentUser\CA
+Import-Certificate -FilePath C:\Temp\rootSSC-01.cer -CertStoreLocation Cert:\CurrentUser\Root
 
 # Signing
 Set-AuthenticodeSignature -FilePath C:\MAPS2403\AdvFunctions\Signing-Script.ps1 -Certificate $cert
