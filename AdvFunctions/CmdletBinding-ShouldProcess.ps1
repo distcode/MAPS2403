a4c37f879fc958ebd3cabdd28bbc1896eacf4d61
distcode <office@dist.at>
2024-03-21 10:35:44 +0100

    SQL Demos initialy added.

---
+++ AdvFunctions/CmdletBinding-ShouldProcess.ps1
@@ -0,0 +1,56 @@
+﻿
+function Do-FooSimple {
+
+    [cmdletbinding(SupportsShouldProcess=$true)]
+    param ()
+
+    if ( $PSCmdlet.ShouldProcess('Writing xxx') ) {
+        
+        # code will be executed without -WhatIf
+        ' xxx '
+
+        #    with -WhatIf --> ShouldProcess() = $False
+        # without -WhatIf --> ShoudlProcess() = $true
+    }
+}
+
+function Do-FooAdv {
+
+    [cmdletbinding(SupportsShouldProcess=$true)]
+    param ()
+
+    if ( $PSCmdlet.ShouldProcess('Host','Writing xxx') ) {
+        
+        # code will be executed without -WhatIf
+        ' xxx '
+
+        #    with -WhatIf --> ShouldProcess() = $False
+        # without -WhatIf --> ShoudlProcess() = $true
+    }
+}
+
+function Do-FooFull {
+
+    [cmdletbinding(SupportsShouldProcess=$true)]
+    param ()
+
+    if ( $PSCmdlet.ShouldProcess('Das Skript wird nun einen Text ausgeben.','Soll das Skript einen Text ausgeben?','Do-FooFull Function') ) {
+        
+        # code will be executed without -WhatIf
+        ' xxx '
+
+        #    with -WhatIf --> ShouldProcess() = $False
+        # without -WhatIf --> ShoudlProcess() = $true
+    }
+}
+
+# Do-FooSimple -WhatIf
+# Do-FooAdv -WhatIf
+
+# Do-FooSimple -Confirm
+# Do-FooAdv -Confirm
+
+# Do-FooFull -WhatIf
+# Do-FooFull -Confirm
+
+Do-FooFull -Verbose
---
+++ SQL/SQL03.ps1
@@ -0,0 +1,33 @@
+﻿
+# Connection to Azure SQL Database
+
+$connectionString = 'Server=tcp:mapssqlsrv-dist.database.windows.net,1433;Initial Catalog=db-dist;Persist Security Info=False;User ID=sqladmin-dist;Password=Pa55w.rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;'
+
+try {
+    $myConn = New-Object -TypeName System.Data.SqlClient.SqlConnection
+    $myConn.ConnectionString = $connectionString
+    $myConn.Open()
+
+    $myDataAdapter = New-Object -TypeName System.Data.SqlClient.SqlDataAdapter('Select * From SalesLT.Customer', $myConn)
+    $myDataTable = New-Object -TypeName System.Data.DataTable
+
+    $RowCount = $myDataAdapter.Fill($myDataTable)
+
+    # $myDataTable
+
+    # $myDataTable | Select-Object -First 10 | Format-Table -Property firstname,lastname,companyname,emailaddress
+    # $myDataTable | Sort-Object -Property CompanyName | Select-Object -First 10 | Format-Table -Property firstname,lastname,companyname,emailaddress
+
+    $myDataTable.Rows[4].LastName
+
+    "Rows: $RowCount"
+
+    "*** FINISHED ***"
+
+    
+}
+catch {
+    $error[0]
+}
+
+$myConn.Close()
